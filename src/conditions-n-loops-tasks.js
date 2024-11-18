7a67a25032d156f41ff0de9c018fee03470aaf83
Kenny-alive <ksvcsv@inbox.ru>
2024-11-18 11:01:16 +0300

    feat: 8 of 19

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -261,8 +261,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+  if (result === str) return true;
+  return false;
 }
 
 /**
